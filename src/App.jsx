f07a1590f156985a704f70828c83f34403725667
jailton costa <jaylton_50@hotmail.com>
2024-03-02 11:55:07 -0300

    tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TextField } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vai por onde ?</h1>
+      <img src="src\vpn.jpeg" className="logoicon" alt="logoproject" />
+      <h1>Vai Por Onde ?</h1>
+      
+      <TextField id="filled-basic" label="Email" variant="filled" /><br />
+      <TextField id="filled-basic" label="Password" variant="filled" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+           Logar {  }
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  
     </>
   )
 }
