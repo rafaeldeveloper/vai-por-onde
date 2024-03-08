62d194e8dadca4d5a3dbd366a3ce47454971b091
TonyLs03 <azulynot@gmail.com>
2024-03-08 17:01:42 -0300

    adicionando chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { useState } from 'react'
+
+import { IoIosSend } from "react-icons/io";
+import { IoTrashBin } from "react-icons/io5";
+import { FaEdit } from "react-icons/fa";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +11,36 @@ import { TextField } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [conversa, setConversa] = useState([]);
+  const [mensagem, setMensagem] = useState('');
+
+  const handleMensagemChange = (event) => {
+    setMensagem(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const novaConversa = {
+      mensagem: mensagem,
+    };
+
+    setConversa([...conversa, novaConversa]);
+    setMensagem('');
+  };
+
+  const handleDeletar = (index) => {
+    const novasConversas = [...conversa];
+    novasConversas.splice(index, 1);
+    setConversa(novasConversas);
+  };
+
+  const handleEditar = (index) => {
+    const conversaEditada = conversa[index];
+    setMensagem(conversaEditada.mensagem);
+    handleDeletar(index);
+  }
 
   return (
     <>
@@ -20,6 +55,29 @@ function App() {
         </button>
       
       </div>
+
+      <h2>Chat</h2>
+
+      <section className='chat-box'>
+
+        <form className='chat-form' onSubmit={handleSubmit}>
+            <label>
+              <input className='chat-input' type='text' value={mensagem} onChange={handleMensagemChange} placeholder='escreva aqui sua mensagem'/>
+              <button className='chat-btn' type="submit" ><IoIosSend /></button>
+            </label>
+        </form>
+
+        <ul className='chat-list'>
+          {conversa.map((conversa, index) => (
+            <li key={index}>
+              {conversa.mensagem}
+              <button className='chat-btn' onClick={() => handleEditar(index)}><FaEdit /></button>
+              <button className='chat-btn' onClick={() => handleDeletar(index)}><IoTrashBin /></button>
+            </li>
+          ))}
+        </ul>
+
+      </section>
   
     </>
   )
